2e0a01ded661176ce6f5cc1ee9e98d0b5ac45231
ajayyadav966171-art <ajayyadav966171@gmail.com>
2026-08-02 13:49:14 +0530

    Remove debug signing configuration and use release config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,12 +31,7 @@ android {
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
-    }
+     }
   }
 
   buildTypes {
@@ -46,7 +41,9 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
-    debug { signingConfig = signingConfigs.getByName("debugConfig") }
+    debug {
+    signingConfig = signingConfigs.getByName("release")
+}
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
